88bc3848aa8d08157d795e09b554e3103ebca6c9
Victor <victor.loures@projetusti.com.br>
2024-03-13 21:40:49 -0300

    Código inicial sem CSS, produtos preenchidos e tabelas criadas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import {
+  produtosIniciaisAlimentos,
+  produtosIniciaisLimpeza,
+} from "./utils/ProdutosIniciais";
+
+const CHAVE_LOCAL_STORAGE_ALIMENTOS = "produtosAlimentosLocalStorage";
+const CHAVE_LOCAL_STORAGE_LIMPEZA = "produtosLimpezaLocalStorage";
 
 function App() {
+  const [produtosAlimentos, setProdutosAlimentos] = useState(
+    produtosIniciaisAlimentos
+  );
+  const [produtosLimpeza, setProdutosLimpeza] = useState(
+    produtosIniciaisLimpeza
+  );
+
+  useEffect(() => {
+    const produtosAlimentosLocalStorage = localStorage.getItem(
+      CHAVE_LOCAL_STORAGE_ALIMENTOS
+    );
+    const produtosLimpezaLocalStorage = localStorage.getItem(
+      CHAVE_LOCAL_STORAGE_LIMPEZA
+    );
+
+    if (produtosAlimentosLocalStorage && produtosLimpezaLocalStorage) {
+      setProdutosAlimentos(produtosAlimentosLocalStorage);
+      setProdutosLimpeza(produtosAlimentosLocalStorage);
+    }
+
+    return () => atualizarLocalStorage();
+  });
+
+  const atualizarLocalStorage = () => {
+    localStorage.removeItem(CHAVE_LOCAL_STORAGE_ALIMENTOS);
+    localStorage.removeItem(CHAVE_LOCAL_STORAGE_LIMPEZA);
+
+    localStorage.setItem(CHAVE_LOCAL_STORAGE_ALIMENTOS, produtosAlimentos);
+    localStorage.setItem(CHAVE_LOCAL_STORAGE_LIMPEZA, produtosLimpeza);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de compras</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "500px",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          <h2>Alimentos</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Ja pegou?</th>
+                <th>Quantidade</th>
+                <th>Produto</th>
+                <th>Preço</th>
+              </tr>
+            </thead>
+            <tbody>
+              {produtosAlimentos.map((prod) => (
+                <tr>
+                  <td>
+                    <input
+                      type="checkbox"
+                      id="jaPegou"
+                      name="jaPegou"
+                      checked={prod.jaPegou}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      id="qte"
+                      name="qte"
+                      min="0"
+                      max="99"
+                      value={prod.qte}
+                    />
+                  </td>
+                  <td>{prod.produto}</td>
+                  <td>
+                    <input
+                      type="number"
+                      id="preco"
+                      name="preco"
+                      value={prod.preco}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>Total:</td>
+                <td>100</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+
+        <div>
+          <h2>Limpeza</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Ja pegou?</th>
+                <th>Quantidade</th>
+                <th>Produto</th>
+                <th>Preço</th>
+              </tr>
+            </thead>
+            <tbody>
+              {produtosLimpeza.map((prod) => (
+                <tr>
+                  <td>
+                    <input
+                      type="checkbox"
+                      id="jaPegou"
+                      name="jaPegou"
+                      checked={prod.jaPegou}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      id="qte"
+                      name="qte"
+                      min="0"
+                      max="99"
+                      value={prod.qte}
+                    />
+                  </td>
+                  <td>{prod.produto}</td>
+                  <td>
+                    <input
+                      type="number"
+                      id="preco"
+                      name="preco"
+                      value={prod.preco}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>Total:</td>
+                <td>100</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
